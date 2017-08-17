aec9f7fb6ea3ebb5e8b2956293f557a053a9d3f4
David Corson-Knowles <david.corsonknowles@gmail.com>
2017-08-17 06:19:33 -0700

    Respect linter in routes

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     resource :session, only: [:create, :destroy]
   end
 
-  root to: "static_pages#root"
+  root to: 'static_pages#root'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
