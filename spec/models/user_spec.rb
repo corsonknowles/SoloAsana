3f057375fe1fd3410975610a883c76a38be1d4f2
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-11-23 15:27:11 -0800

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,7 +89,7 @@ RSpec.describe User, type: :model do
       let(:user2) { create(:user) }
 
       it 'ensures token uniqueness' do
-        allow(:user).to receive(:new_session_token).and_return(user2.session_token)
+        allow(user).to receive(:new_session_token).and_return(user2.session_token)
 
         expect { reset }.to(change { user.session_token })
         expect(user2.session_token).not_to eq(user.session_token)
