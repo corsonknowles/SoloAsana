d9821bc01d30d84ef9a5cc34ed1f7c6fa1178a59
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-11-24 09:54:10 -0800

    Update users_spec.rb

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,23 +10,6 @@ RSpec.describe Api::UsersController, type: :request do
     expect(response).to have_http_status(:ok)
   end
 
-  it "errors on invalid update" do
-    let(:user) { create(:user) }
-    patch "/api/users/#{user.id}", params: { user: { username: "", email: "user@example.com", password: "good_example"} }, headers: headers
-
-    expect(response.content_type).to eq("application/json")
-    expect(response).to have_http_status(:unprocessable_entity)
-  end
-
-  it "accepts valid updates" do
-    let(:user) { create(:user) }
-    patch "/api/users/#{user.id}", params: { user: { username: "My Awesome User", email: "user@example.com", password: "good_example"} }, headers: headers
-
-    expect(response.body).to match("My Awesome User")
-    expect(response.content_type).to eq("application/json")
-    expect(response).to have_http_status(:ok)
-  end
-
   context 'with invalid params' do
     it "does not create a User and renders errors" do
       post "/api/users", params: { user: { username: "My user", email: "user@example.com", password: "bad"} }, headers: headers
@@ -34,6 +17,24 @@ RSpec.describe Api::UsersController, type: :request do
       expect(response.body).to match("Password is too short")
       expect(response.content_type).to eq("application/json")
       expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  context 'with an existing user' do
+    let(:user) { create(:user) }
+    it "errors on invalid update" do
+      patch "/api/users/#{user.id}", params: { user: { username: "", email: "user@example.com", password: "good_example"} }, headers: headers
+
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it "accepts valid updates" do
+      patch "/api/users/#{user.id}", params: { user: { username: "My Awesome User", email: "user@example.com", password: "good_example"} }, headers: headers
+
+      expect(response.body).to match("My Awesome User")
+      expect(response.content_type).to eq("application/json")
+      expect(response).to have_http_status(:ok)
     end
   end
 end
