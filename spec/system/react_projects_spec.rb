3d064744bbec1eb616c51224750a3bda548f4f7e
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-11-27 11:34:56 -0800

    Update react_projects_spec.rb

--- spec/system/react_projects_spec.rb
+++ spec/system/react_projects_spec.rb
@@ -54,21 +54,37 @@ RSpec.describe "React Project Changes", type: :system do
       end
     end
 
-    it 'can delete a 2nd project' do
+    it 'cannot delete the only project' do
+      expect(page).to have_field("project0")
+
+      the_only_project = find_by_id("project0")
+
+      (the_only_project.value.length + 1).times { the_only_project.send_keys [:backspace] }
+      expect(page).to have_field("project0")
+    end
+
+    it 'can create a 2nd project' do
       expect(page).to have_field("project0")
       expect(page).not_to have_field("project1")
+
       fill_in "project0", with: 'This is my new project'
       seeded_project = find_by_id("project0")
       seeded_project.native.send_keys(:return)
 
-      fill_in "project1", with: 'added'
-      newly_entered_project = find_by_id("project1")
-      newly_entered_project.native.send_keys(:return)
-
       expect(page).to have_field("project1")
+    end
 
-      (newly_entered_project.value.length + 1).times { newly_entered_project.send_keys [:backspace] }
-      expect(page).not_to have_selector("project1")
+    context 'with 2 seeded projects' do
+      let!(:second_project) { create(:project, user: user, team: team) }
+
+      it 'can delete a 2nd project' do
+        expect(page).to have_field("project1")
+
+        not_the_only_project = find_by_id("project1")
+        (not_the_only_project.value.length + 1).times { not_the_only_project.send_keys [:backspace] }
+
+        expect(page).not_to have_selector("project1")
+      end
     end
   end
 end
