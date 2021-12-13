64646fb5d9518695c614679b830b459407fc320d
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-12-12 20:25:56 -0800

    Update react_profile_photo_spec.rb

--- spec/system/react_profile_photo_spec.rb
+++ spec/system/react_profile_photo_spec.rb
@@ -13,13 +13,38 @@ RSpec.describe "React Profile Photo", type: :system do
     end
 
     it 'can load the dropzone' do
-      expect(page).to have_text("Welcome Robert")
+      expect(page).to have_text("Welcome #{user.username}")
 
       click_button 'Account'
       find("figure.profile-photo").click
       expect(page).to have_text("Drop an image")
       expect(page).to have_text("Or, click to select a file to upload")
+
       click_on(class: 'inner-modal')
+      click_button 'Update Profile'
+    end
+
+    it 'can close the account modal' do
+      expect(page).to have_text("Welcome #{user.username}")
+      expect(page).not_to have_text("My Profile Settings")
+
+      click_button 'Account'
+      expect(page).to have_text("My Profile Settings")
+
+      click_on(class: 'close-modal')
+      expect(page).not_to have_text("My Profile Settings")
+    end
+
+    it 'can upload files' do
+      click_button 'Account'
+      find("figure.profile-photo").click
+
+      expect(page).to have_css('.inner-modal')
+      expect(page).to have_text("Drop an image")
+      expect(page).to have_text("Or, click to select a file to upload")
+
+      find("div input[type='file']", visible: false).set File.path('public/favicon.ico')
+      expect(page).not_to have_css('.inner-modal')
       click_button 'Update Profile'
     end
   end
