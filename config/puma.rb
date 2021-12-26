e16bf589b20d7fe485da029707ac9c7b38b6c371
Dave Corson-Knowles <dave@chime.com>
2021-12-25 22:30:52 -0800

    Add procfile (#35)

--- config/puma.rb
+++ config/puma.rb
@@ -30,7 +30,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-# preload_app!
+preload_app!
 
 # If you are preloading your application and using Active Record, it's
 # recommended that you close any connections to the database before workers
