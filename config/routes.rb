e7a4caa891cdb23cbada4fe83e724c62decd775f
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-12-25 22:48:12 -0800

    Run rails app update

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,3 @@
 Rails.application.routes.draw do
-
-  namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :destroy, :update, :show]
-    resources :tasks, only: [:create, :destroy, :update]
-    resources :projects, only: [:create, :destroy, :update, :show, :index]
-
-    resource :session, only: [:create, :destroy]
-  end
-
-  root to: 'static_pages#root'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
