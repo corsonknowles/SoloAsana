674e01f96a568409a1f5b3733270d38890d0e56c
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-12-27 23:19:14 -0800

    Correct folders

--- spec/views/api/tasks/show_spec.rb
+++ spec/views/api/tasks/show_spec.rb
@@ -1,19 +1,23 @@
 # frozen_string_literal: true
 
-RSpec.describe Team, type: :view do
-  let(:team) { build(:team) }
+RSpec.describe Task, type: :view do
+  let(:task) { build(:task) }
 
-  it "renders team json" do
-    render partial: "api/teams/team",
+  it "renders task json" do
+    render partial: "api/tasks/task",
            formats: [:json],
-           locals: { team: team }
+           locals: { task: task }
 
-    team_hash = JSON.parse(rendered)
+    task_hash = JSON.parse(rendered)
 
-    expect(team_hash["id"]).to eq(team.id)
-    expect(team_hash["user_id"]).to eq(team.user_id)
-    expect(team_hash["name"]).to eq(team.name)
-    expect(team_hash["created_at"]).to eq(team.created_at)
-    expect(team_hash["updated_at"]).to eq(team.updated_at)
+    expect(task_hash["id"]).to eq(task.id)
+    expect(task_hash["user_id"]).to eq(task.user_id)
+    expect(task_hash["project_id"]).to eq(task.project_id)
+    expect(task_hash["team_id"]).to eq(task.team_id)
+    expect(task_hash["body"]).to eq(task.body)
+    expect(task_hash["done"]).to eq(task.done)
+    expect(task_hash["due"]).to eq(task.due)
+    expect(task_hash["section"]).to eq(task.section)
+    expect(task_hash["title"]).to eq(task.title)
   end
 end
