b48c537bd272ff2939e5df0c2f5edeb46c92ba13
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-12-28 19:51:32 -0800

    Update 20211229034013_add_unique_email_index_to_users.rb

--- db/migrate/20211229034013_add_unique_email_index_to_users.rb
+++ db/migrate/20211229034013_add_unique_email_index_to_users.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class AddUniqueEmailIndexToUsers < ActiveRecord::Migration[6.1]
   def change
-    remove_index :users, name: :index_users_on_email
+    remove_index :users, column: :email, name: :index_users_on_email
     add_index :users, :email, unique: true
   end
 end
