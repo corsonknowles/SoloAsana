cf3e117097348e40671ff53f4cd7fd8d0a4765b6
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-12-29 00:35:46 -0800

    Update application_mailer_spec.rb

--- spec/mailers/application_mailer_spec.rb
+++ spec/mailers/application_mailer_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-RSpec.describe ApplicationMailer, :type => :mailer do
-  it { expect(described_class.default[:from]).to eq 'from@example.com' }
+RSpec.describe ApplicationMailer, type: :mailer do
+  it { expect(described_class.default[:from]).to eq "from@example.com" }
   it { expect(described_class.new.action_has_layout?).to be true }
 end
