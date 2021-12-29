dfb17693acdfb4255e4bdca18af6ba3c72b4a51b
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-12-29 10:09:01 -0800

    Freeze string literals

--- spec/channels/application_cable/channel_spec.rb
+++ spec/channels/application_cable/channel_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 RSpec.describe ApplicationCable::Channel, type: :channel do
   it "subscribes without streams" do
--- spec/channels/application_cable/connection_spec.rb
+++ spec/channels/application_cable/connection_spec.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 RSpec.describe ApplicationCable::Connection, type: :channel do
   it "stubs a user agent" do
