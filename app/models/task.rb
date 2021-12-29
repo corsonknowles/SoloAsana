daf278c96517c86df97fc2824c71155e885291b2
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-12-28 21:47:32 -0800

    Fix all

--- app/models/task.rb
+++ app/models/task.rb
@@ -28,4 +28,7 @@ class Task < ApplicationRecord
   belongs_to :user
   belongs_to :project
   belongs_to :team, optional: true
+
+  validates :user_id, presence: true
+  validates :project_id, presence: true
 end
