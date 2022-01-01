3e73479b52abbd165808dc104908f4613792cfd7
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2021-12-31 20:28:23 -0800

    Create custom_styles.js

---
+++ frontend/styles/custom_styles.js
@@ -0,0 +1,22 @@
+export const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    width                 : '50%',
+    maxWidth              : '575px',
+    minWidth              : '500px',
+    maxHeight             : '585px',
+    height                : '80%',
+    display               : 'flex',
+    justifyContent        : 'center',
+    alignItems            : 'center',
+    color                 : '#49505b',
+    fontWeight            :  'bold',
+    pointerEvents         : 'auto',
+    borderRadius          : '10px'
+  }
+};
