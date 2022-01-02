de3781a4e64a682e86c6f8b7515a39e7fb04f232
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2022-01-02 09:35:02 -0800

    Update react_profile_photo_spec.rb

--- spec/system/react_profile_photo_spec.rb
+++ spec/system/react_profile_photo_spec.rb
@@ -26,6 +26,37 @@ RSpec.describe "React Profile Photo", type: :system do
       click_button "Update Profile"
     end
 
+    it "can open the dropzone file menu" do
+      expect(page).to have_text("Welcome #{user.username}")
+
+      click_button "Account"
+      find("figure.profile-photo").click
+      expect(page).to have_text("Drop an image")
+      expect(page).to have_text("Or, click to select a file to upload")
+
+      find_by_id("profile-dropzone").click
+      # page.execute_script %{ $('input[type="file"]').show(); }
+      # page.execute_script %{ $('input[type="file"]').enable(); }
+      debugger
+
+      page.attach_file('input[type="file"]', File.path("public/apple-touch-icon.png"))
+    end
+
+    it "can reveal the hidden file input" do
+      expect(page).to have_text("Welcome #{user.username}")
+
+      click_button "Account"
+      find("figure.profile-photo").click
+      expect(page).to have_text("Drop an image")
+      expect(page).to have_text("Or, click to select a file to upload")
+
+      page.execute_script %{ $('input[type="file"]').show(); }
+      page.execute_script %{ $('input[type="file"]').enable(); }
+      debugger
+
+      page.attach_file('input[type="file"]', File.path("public/apple-touch-icon.png"))
+    end
+
     it "can close the account modal" do
       expect(page).to have_text("Welcome #{user.username}")
       expect(page).not_to have_text("My Profile Settings")
