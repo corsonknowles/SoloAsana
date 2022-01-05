d0f491c8d2834354fbb75398c24b1d565254d005
Dave Corson-Knowles <david.corsonknowles@gmail.com>
2022-01-04 23:11:41 -0800

    Update react_profile_photo_spec.rb

--- spec/system/react_profile_photo_spec.rb
+++ spec/system/react_profile_photo_spec.rb
@@ -11,74 +11,72 @@ RSpec.describe "React Profile Photo", type: :system do
       fill_in "EMAIL ADDRESS", with: user.email
       fill_in "PASSWORD", with: user.password
 
+      # rubocop:disable RSpec/ExpectInHook
+      # auth the user
       click_button "Sign In"
-    end
-
-    it "can load the dropzone" do
-      expect(page).to have_text("Welcome #{user.username}")
-
-      click_button "Account"
-      find("figure.profile-photo").click
-      expect(page).to have_text("Drop an image")
-      expect(page).to have_text("Or, click to select a file to upload")
-
-      click_on(class: "inner-modal")
-      click_button "Update Profile"
-    end
-
-    it "can open the dropzone file menu" do
-      expect(page).to have_text("Welcome #{user.username}")
-
-      click_button "Account"
-      find("figure.profile-photo").click
-      expect(page).to have_text("Drop an image")
-      expect(page).to have_text("Or, click to select a file to upload")
-
-      find_by_id("profile-dropzone").click
-      # page.execute_script %{ $('input[type="file"]').show(); }
-      # page.execute_script %{ $('input[type="file"]').enable(); }
-      debugger
-
-      page.attach_file('input[type="file"]', File.path("public/apple-touch-icon.png"))
-    end
-
-    it "can reveal the hidden file input" do
-      expect(page).to have_text("Welcome #{user.username}")
-
-      click_button "Account"
-      find("figure.profile-photo").click
-      expect(page).to have_text("Drop an image")
-      expect(page).to have_text("Or, click to select a file to upload")
-
-      page.execute_script %{ $('input[type="file"]').show(); }
-      page.execute_script %{ $('input[type="file"]').enable(); }
-      debugger
-
-      page.attach_file('input[type="file"]', File.path("public/apple-touch-icon.png"))
-    end
-
-    it "can close the account modal" do
       expect(page).to have_text("Welcome #{user.username}")
       expect(page).not_to have_text("My Profile Settings")
 
+      # open the profile modal
       click_button "Account"
       expect(page).to have_text("My Profile Settings")
 
-      click_on(class: "close-modal")
-      expect(page).not_to have_text("My Profile Settings")
-    end
-
-    it "can upload files" do
-      click_button "Account"
+      # open the photo modal
       find("figure.profile-photo").click
-
       expect(page).to have_css(".inner-modal")
       expect(page).to have_text("Drop an image")
       expect(page).to have_text("Or, click to select a file to upload")
+      # rubocop:enable RSpec/ExpectInHook
+    end
 
-      find("div input[type='file']", visible: false).set File.path("public/favicon.ico")
-      expect(page).not_to have_css(".inner-modal")
+    it "can close the dropzone and the account modal" do
+      click_on(class: "inner-modal")
+      expect(page).not_to have_text("Or, click to select a file to upload")
+
       click_button "Update Profile"
+      expect(page).not_to have_text("My Profile Settings")
+    end
+
+    # WARNING: this integration test speaks to Cloudinary
+    # Use Webmock and stub this if you do not want this integration test
+    it "can upload files" do
+      expect do
+        expect(page).not_to have_css("img.profile-photo[src*='cloudinary']")
+
+        file_path = "app/assets/images/favicon/apple-touch-icon.png"
+        find("div input[type='file']", visible: false).set File.path(file_path)
+
+        expect(page).not_to have_css(".inner-modal")
+        expect(page).not_to have_text("Or, click to select a file to upload")
+
+        expect(page).to have_css("img.profile-photo[src*='cloudinary']")
+        ActiveRecord::Base.after_transaction do
+          click_button "Update Profile"
+          expect(page).to have_text("Welcome #{user.username}")
+        end
+      end.to(change { user.reload.photo })
+    end
+
+    it "can open the dropzone file menu" do
+      find_by_id("profile-dropzone").click
+      expect(page).to have_text("Drop an image")
+    end
+
+    it "can reveal and set the hidden file input" do
+      expect do
+        page.execute_script(%{ $('input[type="file"]').show() })
+        file_path = "app/assets/images/favicon/apple-touch-icon.png"
+        find(:file_field).set File.path(file_path)
+
+        expect(page).not_to have_css(".inner-modal")
+        expect(page).not_to have_text("Or, click to select a file to upload")
+
+        expect(page).to have_css("img.profile-photo[src*='cloudinary']")
+        ActiveRecord::Base.after_transaction do
+          click_button "Update Profile"
+          expect(page).to have_text("Welcome #{user.username}")
+        end
+      end.to(change { user.reload.photo })
     end
   end
 end
